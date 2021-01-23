9ebc02753659c1fb57988e1ff2406d5abfa028aa
adrnchk <krollzet@gmail.com>
2021-01-23 13:11:14 +0200

    local libs

--- new_york_times/src/index.tsx
+++ new_york_times/src/index.tsx
@@ -10,12 +10,10 @@ import {Provider} from 'react-redux'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store = {store}>
-    <App/>
-    </Provider>
-    
-    </BrowserRouter>
-    
+      <Provider store = {store}>
+        <App/>
+      </Provider>    
+    </BrowserRouter>    
   </React.StrictMode>,
   document.getElementById('root')
 );
